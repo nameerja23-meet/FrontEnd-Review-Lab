e01713b4257ec92df56656b8b4f9034c80edb09b
nameerja23-meet <nemojabara@gmail.com>
2022-07-25 11:45:37 +0300

    Completed bonuses, Final commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function changeBackgroundColour(){
+	if (document.body.style.backgroundColor == 'rgb(84, 95, 102)'){
+		document.body.style.backgroundColor = "red";
+	}
+	else{
+		document.body.style.backgroundColor = "#545F66";
+	}
+} 
